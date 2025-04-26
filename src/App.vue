83b47fe4b6588a133b83c7897cb3c10db3a0765f
anibal andrade <59257555+androso@users.noreply.github.com>
2025-04-26 15:32:48 +0000

    adding select to show pokemons by type

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,11 @@
 <script setup>
-import Navbar from './components/Navbar.vue';
-import tareas from './components/Tareas.vue'
-import calculadora from './components/calculadora.vue';
-import Pokemon from "./components/PokeApi.vue"
-import axiosFetch from './components/axiosFetch.vue';
-import randomPokemon from './components/randomPokemon.vue';
+import randomPokemon from "./components/randomPokemon.vue";
+import PokemonByType from "./components/PokemonByType.vue";
 </script>
 
 <template>
-  <!-- <axiosFetch/> -->
   <randomPokemon/>
+  <PokemonByType />
 </template>
 
 
